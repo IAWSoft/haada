98066290690f3c706ce27a3f8f17286f3131f6f6
albertm360 <albertmorla@mac.com>
2015-12-18 01:56:23 +0100

    Added index.php

--- index.php
+++ index.php
@@ -3,9 +3,22 @@
 	<head>
 		<title>Welcome! @ HAADA</title>
 		<meta charset="utf-8">
+		<style type="text/css">
+			body
+			{
+				font-family: monospace;
+			}
+		</style>
 	</head>
 	
 	<body>
-
+		<div id="content">
+			<div id="login">
+				<form action="" method="post">
+					<span><p>Email: <input type="text" name="userEmail"></p></span>
+					<span><p>Password: <input type="password" name="userPassword"></p></span>
+				</form>
+			</div>
+		</div>
 	</body>
 </html>
