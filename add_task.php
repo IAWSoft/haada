e0831c5a7bfa2c957fcf8145b96323fec6556bf4
albertm360 <albertmorla@mac.com>
2016-01-16 20:07:56 +0100

    add_task modified

--- add_task.php
+++ add_task.php
@@ -28,23 +28,15 @@
 					<form action="" method="post">
 						<p>Task Name: <input type="text" name="taskName"></p>
 						<p>Category: <span><select name="category">';
-						printCategories();
 
-						function printCategories()
+						require("includes/config.php");
+						$q = "SELECT categoryId,categoryName from category";
+						$r = mysqli_query($cdb, $q);
+
+						while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC))
 						{
-							require("includes/config.php");
-							$q = "SELECT categoryId,categoryName from category";
-							$r = mysqli_query($cdb, $q);
-							$printedCategories = '';
-
-							while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC))
-							{
-								$printedCategories .= '<option value="'. $row['categoryId'] . '">' . $row['categoryName'] . '</option>';
-							}
-
-							return $printedCategories;
+							echo '<option value="'. $row['categoryId'] . '">' . $row['categoryName'] . '</option>';
 						}
-						
 
 						'</select></span></p>
 						<p>Description: <textarea name="description"></textarea></p>
